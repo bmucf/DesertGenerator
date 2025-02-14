830e8c8e8a9f93c8bc6a05ceb58079751e211f93
bmucf <br407069@ucf.edu>
2025-02-14 16:21:45 -0500

    work

--- Assets/Scripts/DesertScene.cs
+++ Assets/Scripts/DesertScene.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DesertScene : MonoBehaviour
 {
-    public int baseSize;
+    public int baseDimension;
     public float stoneOffset;
+    public GameObject[] stones;
 
     private int stonesRequired;
 
@@ -27,24 +29,40 @@ public class DesertScene : MonoBehaviour
 
     void InitializeVariables()
     {
-        for (int i = baseSize; i > 0; i--)
+        int length = baseDimension;
+        int width = baseDimension;
+        int height = baseDimension;
+
+        for (int i = baseDimension; i > 0; i--)
         {
-            stonesRequired = stonesRequired + (baseSize * baseSize);
-            baseSize--;
+            stonesRequired = stonesRequired + (baseDimension * baseDimension);
+            baseDimension--;
         }
 
         Debug.Log("Total Number of Stones: " + stonesRequired);
+    }
+
+    void CreateGround()
+    {
+
     }
 
     void CreatePyramid()
     {
         GameObject pyramidParent = new GameObject("PyramidParent");
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < stonesRequired; i++)
         {
+            for(int x = 0; x < )
             GameObject stone = GameObject.CreatePrimitive(PrimitiveType.Cube);
             stone.transform.parent = pyramidParent.transform;
             stone.transform.position = new Vector3(i * stoneOffset, 0, 0);
+            for (int j = 0; j < 4; j++)
+            {
+                GameObject stone2 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                stone2.transform.parent = pyramidParent.transform;
+                stone2.transform.position = new Vector3(0, 0, i * stoneOffset);
+            }
         }
     }
 }
